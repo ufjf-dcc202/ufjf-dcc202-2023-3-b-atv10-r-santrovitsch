97c9b87cb156cabe977e315b1aa458f1aca1a41b
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-04 12:49:30 -0300

    editei main para chamar função

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import { getJoao, getMaria } from "./joaoEMaria.js"
+import { deJoaoParaMaria, getJoao, getMaria } from "./joaoEMaria.js"
 
 // main.js
 console.log('módulo main');
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
+
+deJoaoParaMaria();
 
 console.log('João tem ', getJoao(), ' maçãs!');
 console.log('Maria tem ', getMaria(), ' maçãs!');
